dab1e808876d44a350c131557eb42129f6495f6e
Afanyuy Caleb <afanyuys@gmail.com>
2023-12-30 02:04:43 +0100

    First commit for the cart Project

---
+++ New_Project/project.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Generating html with JS</title>
+</head>
+<body>
+  
+  <img src="<?= $img_path?>/Orange.jpg" alt="img here" height="40px" width="40px">
+  <script src="index.js"></script>
+
+</body>
+</html>
